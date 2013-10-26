f68ba7b500e775ae04fedc45ca4ec16339bb3c15
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-25 22:20:00 -0400

    added loaded die module

--- player.rb
+++ player.rb
@@ -1,4 +1,8 @@
+require_relative 'playable'
+
 class Player
+  include Playable
+
   attr_accessor :name
   attr_reader :health
 
@@ -33,20 +37,6 @@ class Player
 
   def to_s
     "I'm #{@name} with a health = #{@health}, points = #{points}, and a score = #{score}."
-  end
-
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
-
-  def strong?
-    @health > 100
   end
 
   def <=>(other)
