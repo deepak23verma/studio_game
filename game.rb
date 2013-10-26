46ae3b19a7f93047fe0624ce39e8412073866fef
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-25 20:11:22 -0400

    fixed high_scores.txt to high_scores.csv

--- game.rb
+++ game.rb
@@ -21,7 +21,7 @@ class Game
     end
   end
 
-  def save_high_scores(to_file="high_scores.txt")
+  def save_high_scores(to_file="high_scores.csv")
     File.open(to_file, "w") do |file|
       file.puts "#{@title} High Scores:"
       @players.sort.each do |player| 
